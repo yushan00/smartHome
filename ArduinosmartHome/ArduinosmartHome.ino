aebfa08f41ae5a7b7c8e61728815d1ed7b8fbdb8
yushan00 <Yushanx32@gmail.com>
2020-09-28 16:22:04 +0800

    依狀態不同送出 alert, normal 訊息給控制器

--- ArduinosmartHome/ArduinosmartHome.ino
+++ ArduinosmartHome/ArduinosmartHome.ino
@@ -9,7 +9,7 @@ void setup() {
 
 void loop() {
   if(digitalRead(2)==HIGH) {
-   
+  Serial.println("alert");
   digitalWrite(12, HIGH);
   digitalWrite(11, LOW);
   tone(7, 1000, 500);
@@ -20,7 +20,9 @@ void loop() {
   delay(500);
   }
   else {
+  Serial.println("normal");
   digitalWrite(12, LOW);
   digitalWrite(11, LOW);
+  delay(1000);
   }
 }

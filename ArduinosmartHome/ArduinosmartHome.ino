8a208144af01090808fd9e34641dd6509148e43d
yushan00 <Yushanx32@gmail.com>
2020-09-28 15:26:28 +0800

    增加一顆警示燈,交替閃爍

--- ArduinosmartHome/ArduinosmartHome.ino
+++ ArduinosmartHome/ArduinosmartHome.ino
@@ -1,12 +1,15 @@
 void setup() {
  pinMode(12, OUTPUT);
-
+ pinMode(11, OUTPUT);
 }
 
 void loop() {
   digitalWrite(12, HIGH);
+  digitalWrite(11, LOW);
   delay(500);
   digitalWrite(12, LOW);
+  digitalWrite(11, HIGH);
+ 
   delay(500);
 
 }

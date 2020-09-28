a1b5530bbd730a368982168317154d6bbfb6b510
yushan00 <Yushanx32@gmail.com>
2020-09-28 15:55:59 +0800

    修改為斷線時才發警報

--- ArduinosmartHome/ArduinosmartHome.ino
+++ ArduinosmartHome/ArduinosmartHome.ino
@@ -2,11 +2,14 @@ void setup() {
  pinMode(12, OUTPUT);
  pinMode(11, OUTPUT);
  pinMode(7, OUTPUT);
- 
+ pinMode(2, INPUT);
+ Serial.begin(9600);
  
 }
 
 void loop() {
+  if(digitalRead(2)==HIGH) {
+   
   digitalWrite(12, HIGH);
   digitalWrite(11, LOW);
   tone(7, 1000, 500);
@@ -15,4 +18,5 @@ void loop() {
   digitalWrite(11, HIGH);
   tone(7, 800, 500);
   delay(500);
+  }
 }
